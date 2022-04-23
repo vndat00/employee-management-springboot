8f5d9b70c6298db33af9e9d488507e98ff6b8aa8
vndat00 <ngocdat1908@gmail.com>
2022-04-23 15:22:41 +0700

    create project architecture

---
+++ src/main/java/net/vndat00/springboot/SpringbootBackendApplication.java
@@ -0,0 +1,13 @@
+package net.vndat00.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBackendApplication.class, args);
+    }
+
+}

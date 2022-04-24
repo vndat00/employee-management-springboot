d94a58264606c3421744d4b7e52e9d8fd38a3585
vndat00 <ngocdat1908@gmail.com>
2022-04-25 00:43:40 +0700

    add CrossOrigin annotation in EmployeeController file

--- src/main/java/net/vndat00/springboot/controller/EmployeeController.java
+++ src/main/java/net/vndat00/springboot/controller/EmployeeController.java
@@ -3,12 +3,14 @@ package net.vndat00.springboot.controller;
 import net.vndat00.springboot.model.Employee;
 import net.vndat00.springboot.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:3000"})
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeController {
@@ -21,7 +23,4 @@ public class EmployeeController {
     public List<Employee> getAllEmployees(){
         return employeeRepository.findAll();
     }
-
-
-
 }
